091f17dd39d03a94d4bb12e0feb0396c278b2144
BansheePrime <cmapuk-xpehob@yandex.ru>
2024-02-17 22:46:59 +0300

    stil thinking on concept

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Project.FoundryVTT_in_Docker
 FoundryVTT in Docker
+
+## FoundryVTT as VM
+Testing and development
+
+Stack:
+VM provisioning and management: vagrant - virtualbox - ansible
+VM: ubuntu 22.04 - ufw - node.js - nginx as reverse proxy
+backup: rsync(?)
+
+
+### In perspective
+CI/CD this
--- VM_turntable/check.sh
+++ VM_turntable/check.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo 'port 80 --- ' $(curl -I --silent 192.168.56.49 | awk 'NR==3')
-echo 'port 30000 --- ' $(curl -I --silent 192.168.56.49:30000 2>/dev/null | sed -n '3 p')
+echo 'port 80 --- ' $(curl -I --silent 192.168.56.48 | awk 'NR==3')
+echo 'port 30000 --- ' $(curl -I --silent 192.168.56.48:30000 | sed -n '3 p')
